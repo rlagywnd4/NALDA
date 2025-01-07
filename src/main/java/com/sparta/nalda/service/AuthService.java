1d65ad3e72f5f98df8825ffee24f17b3b5cb2719
DeaHyun0911 <eogus0911@gmail.com>
2025-01-08 00:00:21 +0900

    feat: hash password
    - 비밀번호 암호화하여 저장

--- src/main/java/com/sparta/nalda/service/AuthService.java
+++ src/main/java/com/sparta/nalda/service/AuthService.java
@@ -4,6 +4,7 @@ import com.sparta.nalda.common.UserRole;
 import com.sparta.nalda.entity.UserEntity;
 import com.sparta.nalda.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,12 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void signup(String email, String password, String address, UserRole userRole) {
 
-        UserEntity user = new UserEntity(email, password, address, userRole);
+        String hashPassword = passwordEncoder.encode(password);
+        UserEntity user = new UserEntity(email, hashPassword, address, userRole);
         userRepository.save(user);
 
     }
