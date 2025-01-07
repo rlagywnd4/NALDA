cd35dcbe51d53c601add5acff106ee972c0d3e41
DeaHyun0911 <eogus0911@gmail.com>
2025-01-07 23:41:03 +0900

    feat: signup add
    - 회원가입 기능 추가

---
+++ src/main/java/com/sparta/nalda/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.nalda.repository;
+
+import com.sparta.nalda.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
