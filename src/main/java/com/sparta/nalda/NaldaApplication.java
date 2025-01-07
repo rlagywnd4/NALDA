95d2e25bb2af6b786d168c2b2303f0837b13d626
rlagywnd4 <rlagywnd4@gmail.com>
2025-01-07 21:30:55 +0900

    first commit

---
+++ src/main/java/com/sparta/nalda/NaldaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nalda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaldaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaldaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/nalda/NaldaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nalda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaldaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
