ab15fa3853cec480b788718a23ef58ffda1343c3
ehdrjs4802 <ehdrjs4802@gmail.com>
2025-01-08 21:29:06 +0900

    refactor : refactoring

--- src/main/java/com/sparta/nalda/controller/OwnerMenuController.java
+++ src/main/java/com/sparta/nalda/controller/OwnerMenuController.java
@@ -7,7 +7,6 @@ import com.sparta.nalda.dto.menu.UpdateMenuRequestDto;
 import com.sparta.nalda.service.menu.MenuService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.sql.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
