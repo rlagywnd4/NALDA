ecadb84cab51a58a685b334a6e0d5eb8443ddf40
DeaHyun0911 <eogus0911@gmail.com>
2025-01-08 19:52:45 +0900

    feat: SecurityConfig
    - AuthenticationManager Bean 등록
    - session 생성 기능 OFF

--- src/main/java/com/sparta/nalda/config/SecurityConfig.java
+++ src/main/java/com/sparta/nalda/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package com.sparta.nalda.config;
 
+import com.sparta.nalda.util.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -17,12 +23,30 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * AuthenticationManager Bean 등록
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    /**
+     * 개발단계 : 모든 경로 인증없이 접근 가능하도록 임시로 설정
+     * CSRF 보호 기능 OFF
+     * 세션을 생성하지 않도록 설정
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable());
-        http.authorizeHttpRequests((authorize) ->
+        http
+            .csrf((csrf) -> csrf.disable())
+            .authorizeHttpRequests((authorize) ->
             authorize.requestMatchers("/**").permitAll()
-        );
+        )
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
+
         return http.build();
     }
 
