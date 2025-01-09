e75d667329dc88a7d171d3d61aa87a3633a34cc8
DeaHyun0911 <eogus0911@gmail.com>
2025-01-09 16:07:00 +0900

    feat: withdraw user signup block
    - 탈퇴한 회원 재가입 방지

--- src/main/java/com/sparta/nalda/repository/WithDrawnEmailRepository.java
+++ src/main/java/com/sparta/nalda/repository/WithDrawnEmailRepository.java
@@ -4,4 +4,6 @@ import com.sparta.nalda.entity.WithDrawnEmailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WithDrawnEmailRepository extends JpaRepository<WithDrawnEmailEntity, Long> {
+
+    boolean existsByEmail(String email);
 }
