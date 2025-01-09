2f84fac56e477ce83ee9d3e827de7951066e3237
DeaHyun0911 <eogus0911@gmail.com>
2025-01-09 19:53:55 +0900

    recfactor: Remove duplicate code
    - requestDto 검증 로직 중복코드 제거

--- src/main/java/com/sparta/nalda/dto/user/PasswordUpdateRequestDto.java
+++ src/main/java/com/sparta/nalda/dto/user/PasswordUpdateRequestDto.java
@@ -1,5 +1,7 @@
 package com.sparta.nalda.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +9,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PasswordUpdateRequestDto {
 
+    @NotBlank
+    @Pattern(regexp = UserValid.PASSWORD_REGEX)
     private final String oldPassword;
+
+    @NotBlank
+    @Pattern(regexp = UserValid.PASSWORD_REGEX)
     private final String newPassword;
 
 }
