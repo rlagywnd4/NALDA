346b6b4c08fba4ad2d13ce50c6e00824a3d3191f
rlagywnd4 <71661011+rlagywnd4@users.noreply.github.com>
2025-01-09 21:47:45 +0900

    fix: deleted method return
    머지중 삭제된 메소드 긴급 복구

--- src/main/java/com/sparta/nalda/repository/StoreRepository.java
+++ src/main/java/com/sparta/nalda/repository/StoreRepository.java
@@ -1,7 +1,14 @@
 package com.sparta.nalda.repository;
 
 import com.sparta.nalda.entity.StoreEntity;
+import com.sparta.nalda.entity.UserEntity;
+import com.sparta.nalda.util.StoreStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
+    int countByUserAndStatus(UserEntity user, StoreStatus storeStatus);
+
+    Page<StoreEntity> findByStoreNameContaining(String searchKeyword, Pageable pageable);
 }
