67ecd6de58aafb20a55f77207bdf60ca4089d897
DeaHyun0911 <eogus0911@gmail.com>
2025-01-09 15:26:40 +0900

    recfactor: code Optimization
    - 중복 코드 제거

--- src/main/java/com/sparta/nalda/service/user/UserServiceImpl.java
+++ src/main/java/com/sparta/nalda/service/user/UserServiceImpl.java
@@ -16,22 +16,22 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    public UserResponseDto getUser(Long id) {
-        UserEntity user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("User not found"));
+    public UserResponseDto getUser(Long userId) {
+        UserEntity user = userRepository.findByIdOrElseThrow(userId);
         return new UserResponseDto(user.getEmail(), user.getUserRole(), user.getAddress());
     }
 
     @Override
     @Transactional
-    public void updateUserAddress(Long id, String address) {
-        UserEntity user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("User not found"));
+    public void updateUserAddress(Long userId, String address) {
+        UserEntity user = userRepository.findByIdOrElseThrow(userId);
         user.updateAddress(address);
     }
 
     @Override
     @Transactional
     public void updateUserPassword(Long userId, String oldPassword, String newPassword) {
-        UserEntity user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        UserEntity user = userRepository.findByIdOrElseThrow(userId);
 
         if(passwordEncoder.matches(newPassword, user.getPassword())){
             throw new IllegalArgumentException("새 비밀번호는 기존 비밀번호와 같을 수 없습니다.");
