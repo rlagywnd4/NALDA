d04695cfce8a1ed1bfaad9653a337908679cef04
DeaHyun0911 <eogus0911@gmail.com>
2025-01-10 17:16:46 +0900

    feat: OrderLogging create
    - 주문 생성 시 로깅

--- src/test/java/com/sparta/nalda/controller/UserTest.java
+++ src/test/java/com/sparta/nalda/controller/UserTest.java
@@ -1,14 +1,20 @@
 package com.sparta.nalda.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.nalda.common.MessageResponse;
 import com.sparta.nalda.dto.user.SignupRequestDto;
 import com.sparta.nalda.util.UserRole;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -32,12 +38,19 @@ class UserTest {
             "address",
             UserRole.CUSTOMER);
 
-
-
-
         //when
+        ResultActions result = this.mockMvc.perform(
+            post("/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signupRequestDto)))
+        );
 
         //then
+        String contentAsString = result.andReturn().getResponse().getContentAsString();
+        MessageResponse actualResult = this.objectMapper.readValue(contentAsString, MessageResponse.class);
+
+        result.andExpect(status().isOk());
+
     }
 
     @Test
